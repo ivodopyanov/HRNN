1d4d027dcdb262cdf2df388e4ed77c74827904e4
Igor Vodopyanov <ivodopyanov@naumen.ru>
2017-03-28 17:34:14 +0500

    1

--- Encoder/Encoder_RL_layer.py
+++ Encoder/Encoder_RL_layer.py
@@ -75,7 +75,7 @@ class Encoder_RL_Layer(Layer):
             B_action = K.cast_to_floatx(1.)
 
         policy = activations.relu(K.dot(x*B_W, self.W_action_1) + K.dot(h_tm1*B_U, self.U_action_1) + self.b_action_1)
-        policy = K.dot(policy*B_action, self.W_action_2)+self.b_action_2
+        policy = activations.relu(K.dot(policy*B_action, self.W_action_2)+self.b_action_2)
         policy = K.exp(K.minimum(K.dot(policy*B_action, self.W_action_3)+self.b_action_3,5))
 
         return policy
